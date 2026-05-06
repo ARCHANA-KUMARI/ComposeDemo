0c17ec5138c4e88b27193ab65a36f22d7755eb28
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-05-06 21:09:57 +0530

    3. Image properties

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -57,4 +61,11 @@ fun GreetingPreview2() {
     ComposeDemoTheme {
         Greeting("Android Archana. Preview 2.......")
     }
+}
+
+
+@Preview(showBackground = true, name = "Preview 3" , widthDp = 300, heightDp = 500 )
+@Composable
+fun GreetingPreview3() {
+    Image(painter = painterResource(R.drawable.ic_launcher_background), "preview 3 background image", colorFilter = ColorFilter.tint(Color.Red) )
 }
